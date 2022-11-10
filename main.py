0030bc7dfdcce22d924cc2cb3cc7ac5d44d4eed6
Gordeyev <Aleksey.Gordeyev@METRAN.local>
2022-11-10 10:44:49 +0500

    added file from regex hometask to check if decorator works properly

--- main.py
+++ main.py
@@ -1,60 +1,54 @@
-import os
-import datetime
+import re
+import csv
+from logger import logger
 
-def logger(path):    
-    def __logger(old_function):
-        
+@logger('main.log')
+def get_list_from_file(raw_file_name:str):
+    '''returns a list containing each line from csv'''
+    with open(raw_file_name, 'r', encoding="utf-8") as f:
+        return list(csv.reader(f, delimiter=","))
 
-        def new_function(*args, **kwargs):
-            
-            result = float(old_function(*args, **kwargs))
-            
-            if not os.path.exists(path): open(path, 'x').close()
-                
-            with open(path, 'a') as log_file:
-                log_file.write(f'{datetime.datetime.now()}, {old_function.__name__}, {args}, {kwargs}, {result} \n')
-            
-            if result // 1 == result: result = int(result)
-            
-            return result
-        
-        return new_function
+@logger('main.log')
+def correct_full_name(last_name:str, first_name:str, middle_name:str):
+    '''recieves raw inputs and returns a list - [last name, first name, middle name]'''
+    full_name = (last_name + ' ' + first_name + ' ' + middle_name).split()
+    while len(full_name) != 3:
+        full_name.append('')
+    return full_name
+
+@logger('main.log')
+def get_data_to_record(raw_file_name:str):
+    '''transforms raw data from csv file to list of lists with uniformed records'''
+    contacts_list = get_list_from_file(raw_file_name)
+    result_dic = {lastname: ['' for _ in range(7)] for lastname in [line[0].split()[0] for line in contacts_list]} #creates a dict {last_name:[data]} for every record in csv
+    for raw_record in contacts_list:
+        full_name = correct_full_name(raw_record[0], raw_record[1], raw_record[2])
+        fine_record = full_name + [raw_record[3], raw_record[4], uniform_phoes(raw_record[5]), raw_record[6]]
+        for i, element in enumerate(fine_record):
+            if not result_dic[full_name[0]][i]:
+                result_dic[full_name[0]][i] = element
+    return [record for record in result_dic.values()]
+
+@logger('main.log')
+def uniform_phoes(raw_phone:str):
+    '''transformes phone number from csv into "+7(999)999-99-99" or "+7(999)999-99-99 доб.9999" format'''
+    phone_pattern = r'(\+7|8)?[\s\(]*(\d{3})[\s\)-]*(\d{3})[\s\)-]*(\d{2})[\s\)-]*(\d{2})\s?\(?(доб)?.?\s?(\d{4})?\)?'
+    if 'доб' in raw_phone:
+        return re.sub(phone_pattern, r"+7(\2)\3-\4-\5 \6.\7", raw_phone)
+    else:
+        return re.sub(phone_pattern, r"+7(\2)\3-\4-\5", raw_phone)
+
+@logger('main.log')    
+def record_data_to_csv(data:list, fine_file_name):
+    '''records a list with data to csv file'''
+    with open(fine_file_name, "w", encoding='utf-8', newline='') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(data)
+
+@logger('main.log')
+def make_contacts_similar(raw_file_name:str, fine_file_name:str):
+    result = get_data_to_record(raw_file_name)
+    record_data_to_csv(result, fine_file_name)
     
-    return __logger
-
-def test_2():
-    paths = ('log_1.log', 'log_2.log', 'log_3.log')
-
-    for path in paths:
-        if os.path.exists(path):
-            os.remove(path)
-
-        @logger(path)
-        def summator(a, b=0):
-            return a + b
-
-        @logger(path)
-        def div(a, b):
-            return a / b
-
-        result = summator(2, 2)
-        assert isinstance(result, int), 'Должно вернуться целое число'
-        assert result == 4, '2 + 2 = 4'
-        div(4, 2)
-        summator(4.3, b=2.2)
-
-    for path in paths:
-
-        assert os.path.exists(path), f'файл {path} должен существовать'
-
-        with open(path) as log_file:
-            log_file_content = log_file.read()
-
-        assert 'summator' in log_file_content, 'должно записаться имя функции'
-
-        for item in (4.3, 2.2, 6.5):
-            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
-
-
-if __name__ == '__main__':
-    test_2()
+if __name__ =='__main__':
+    make_contacts_similar("phonebook_raw.csv", "phonebook.csv")
